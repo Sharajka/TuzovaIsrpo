a03d7212ddd5fd1c8f62d26626db7931ab6b6545
Dmitrii <drozdovdm27@gmail.com>
2022-02-09 16:26:25 +0300

    Start lab 7

---
+++ Lab7/PizzaFilling.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class PizzaFilling
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public static List<PizzaFilling> GenerateTestData()
+        {
+            return new List<PizzaFilling>()
+            {
+                new PizzaFilling()
+                {
+                    Name = "Моцарелла",
+                    Price = 30
+                },
+                new PizzaFilling()
+                {
+                    Name = "Ветчина",
+                    Price = 40
+                },
+                new PizzaFilling()
+                {
+                    Name = "Грибы",
+                    Price = 20
+                },
+                new PizzaFilling()
+                {
+                    Name = "Огурцы",
+                    Price = 10
+                },
+                new PizzaFilling()
+                {
+                    Name = "Индейка",
+                    Price = 70
+                },
+                new PizzaFilling()
+                {
+                    Name = "Оливки",
+                    Price = 15
+                },
+                new PizzaFilling()
+                {
+                    Name = "Салями",
+                    Price = 12
+                },
+                new PizzaFilling()
+                {
+                    Name = "Помидоры",
+                    Price = 6
+                },
+                new PizzaFilling()
+                {
+                    Name = "Ананас",
+                    Price = 6000000000
+                }
+            };
+        }
+    }
+}
---
+++ Lab7/SelectFillingPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task1
+{
+    /// <summary>
+    /// Interaction logic for SelectFillingPage.xaml
+    /// </summary>
+    public partial class SelectFillingPage : Page
+    {
+        private readonly List<PizzaFilling> avaliblePizzaFillings = new List<PizzaFilling>();
+        private readonly List<PizzaFilling> settedPizzaFillings = new List<PizzaFilling>();
+
+        public SelectFillingPage(List<PizzaFilling> selectedPizzaFillings)
+        {
+            InitializeComponent();
+
+            var avaliblePizzaFillings = PizzaFilling.GenerateTestData();
+            avaliblePizzaFillings.RemoveAll(item =>
+                    selectedPizzaFillings.Any(seleceted => seleceted.Name == item.Name)
+                );
+
+            this.avaliblePizzaFillings = avaliblePizzaFillings;
+            this.settedPizzaFillings = selectedPizzaFillings;
+            SyncListsWithView();
+        }
+
+        private void SyncListsWithView()
+        {
+            avalivableFillingsListView.ItemsSource = avaliblePizzaFillings;
+            settedFillingsListView.ItemsSource = settedPizzaFillings;
+        }
+
+        private void addFilling_Click(object sender, RoutedEventArgs e)
+        {
+            if (avalivableFillingsListView.SelectedItem == null)
+                return;
+
+            var item = settedFillingsListView.SelectedItem as PizzaFilling;
+            settedPizzaFillings.Remove(item);
+            avaliblePizzaFillings.Add(item);
+            SyncListsWithView();
+        }
+
+        private void removeFilling_Click(object sender, RoutedEventArgs e)
+        {
+            if (settedFillingsListView.SelectedItem == null)
+                return;
+
+            var item = settedFillingsListView.SelectedItem as PizzaFilling;
+            settedPizzaFillings.Add(item);
+            avaliblePizzaFillings.Remove(item);
+            SyncListsWithView();
+        }
+
+        private void addAllSelectedFillingsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
