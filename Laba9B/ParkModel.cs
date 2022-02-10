abd2f15b0e2dd1b3c837033ddf8b04bd4e080d7a
JoReksar <kokin.ru@gmail.com>
2022-02-10 03:04:32 +0300

    Validation

---
+++ Laba9B/ParkModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba9B
+{
+    internal class ParkModel : IDataErrorInfo
+    {
+        public static MainWindow Owner { get; set; }
+
+
+        public int CountAutomobile { get; set; }
+
+        private string _error;
+        public string Error => _error;
+
+
+        public string this[string columnName]
+        {
+            get
+            {
+                _error = string.Empty;
+
+                switch (columnName)
+                {
+                    case nameof(CountAutomobile):
+                        if (CountAutomobile >= 2 && CountAutomobile <= 60)
+                        {
+                            Owner.ConfirmButton.IsEnabled = true;
+                            break;
+                        }
+
+                        _error = "Введите значение от 2 до 60";
+                        Owner.ConfirmButton.IsEnabled = false;
+                        break;
+                }
+
+                return Error;
+            }
+        }
+
+        
+    }
+}
